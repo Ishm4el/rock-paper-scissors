9e6018cfa519a95b2694a9cdc0bd5e704cbc53c4
Ish <ismaelv@protonmail.com>
2023-03-20 01:40:16 -0700

    Implemented completely functing buttons that run the game. Next assignment is to enhance the GUI

--- rock-paper-scissors-code.js
+++ rock-paper-scissors-code.js
@@ -11,15 +11,15 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function lose(playerSelection, computerSelection) {
-    return { response: `You lose! ${computerSelection} beats ${playerSelection}`, add: 0};
+    return { response: `You lose! ${computerSelection} beats ${playerSelection}`, add: 0, cAdd: 1};
 }
 
 function win(playerSelection, computerSelection) {
-    return {response: `You win! ${playerSelection} beats ${computerSelection}`, add: 1};
+    return {response: `You win! ${playerSelection} beats ${computerSelection}`, add: 1, cAdd: 0};
 }
 
 function tie(playerSelection, computerSelection) {
-    return {response: `It's a tie! ${playerSelection} equally matches ${computerSelection}`, add: 0};
+    return {response: `It's a tie! ${playerSelection} equally matches ${computerSelection}`, add: 0, cAdd: 0};
 }
 
 function getComputerChoice() {
@@ -51,13 +51,13 @@ function getUserChoice(userChoice) {
 }
 
 function game() {
-    let playerWins = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getUserChoice(prompt());
+    let playerWins = 0, computerWins = 0;
+    while(playerWins < 5 || computerWins < 5) {
         const computerSelection = getComputerChoice();
         result = playRound(playerSelection, computerSelection);
         console.log(result.response);
         playerWins += result.add;
+        computerWins += result.cAdd;
     }
     if (playerWins > 2) {
         console.log("You won this game!");
@@ -67,4 +67,49 @@ function game() {
     }
 }
 
-game();
+let pWins = 0, cWins = 0;
+
+const container = document.querySelector('#container');
+
+const playerScore = document.createElement('h3');
+playerScore.textContent = "Player Score: " + pWins;
+container.appendChild(playerScore);
+
+const computerScore = document.createElement('h3');
+computerScore.textContent = "Computer Score: " + cWins;
+container.appendChild(computerScore);
+
+const response = document.createElement('h1');
+container.appendChild(response);
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (container.contains(response)) {
+            container.removeChild(response);
+        }
+
+        result = playRound(getUserChoice(button.id), getComputerChoice());
+        console.log(result.response);
+        pWins += result.add;
+        cWins += result.cAdd;
+
+        if (pWins == 5) {
+            response.textContent = "You won this match!";
+            container.insertBefore(response, playerScore);
+            pWins = 0, cWins = 0;
+        } 
+        else if (cWins == 5) {
+            response.textContent = "The computer has won this match!";
+            pWins = 0, cWins = 0;
+        }
+        else {
+            container.removeChild(playerScore);
+            container.removeChild(computerScore);
+            playerScore.textContent = "Player Score: " + pWins;
+            computerScore.textContent = "Computer Score: " + cWins;
+            container.appendChild(playerScore);
+            container.appendChild(computerScore);
+        }
+    });
+});
