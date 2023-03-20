3711bebc4e36af8bca430ccfd38ab883b5763f10
Ish <ismaelv@protonmail.com>
2023-03-20 03:49:42 -0700

    Developed a pleasing GUI. Can be improved by adding images, a header and a footer. But for now, it is a clean simple web game.

--- rock-paper-scissors-code.js
+++ rock-paper-scissors-code.js
@@ -50,66 +50,76 @@ function getUserChoice(userChoice) {
     }
 }
 
-function game() {
-    let playerWins = 0, computerWins = 0;
-    while(playerWins < 5 || computerWins < 5) {
-        const computerSelection = getComputerChoice();
-        result = playRound(playerSelection, computerSelection);
-        console.log(result.response);
-        playerWins += result.add;
-        computerWins += result.cAdd;
-    }
-    if (playerWins > 2) {
-        console.log("You won this game!");
-    }
-    else {
-        console.log("The computer has won..");
-    }
-}
-
 let pWins = 0, cWins = 0;
 
 const container = document.querySelector('#container');
 
+// will log the response per round
+const action = document.createElement('h2');
+container.append(action);
+
+// keeps player score
 const playerScore = document.createElement('h3');
 playerScore.textContent = "Player Score: " + pWins;
 container.appendChild(playerScore);
 
+// keeps computer score
 const computerScore = document.createElement('h3');
 computerScore.textContent = "Computer Score: " + cWins;
 container.appendChild(computerScore);
 
+// will announce the winner per match
 const response = document.createElement('h1');
-container.appendChild(response);
+
+// hold the visual graph data
+const playerGraph = document.querySelector('#player');
+const computerGraph = document.querySelector('#computer');
+const playerGraphText = document.querySelector('#player-text');
+const computerGraphText = document.querySelector('#computer-text');
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if (container.contains(response)) {
             container.removeChild(response);
+            playerGraphText.textContent = "Player";
+            computerGraphText.textContent = "Computer";
         }
 
         result = playRound(getUserChoice(button.id), getComputerChoice());
-        console.log(result.response);
+        
         pWins += result.add;
         cWins += result.cAdd;
 
+        action.textContent = result.response;
+
+        playerScore.textContent = "Player Score: " + pWins;
+        computerScore.textContent = "Computer Score: " + cWins;
+
+        playerGraph.style.cssText = `flex: ${pWins} 1 auto;`;
+        computerGraph.style.cssText = `flex: ${cWins} 1 auto;`;
+        if (pWins == 0 && cWins == 0) {
+            playerGraph.style.cssText = `flex: 1 1 auto;`;
+            computerGraph.style.cssText = `flex: 1 1 auto;`;
+        }
+
         if (pWins == 5) {
             response.textContent = "You won this match!";
-            container.insertBefore(response, playerScore);
+            container.insertBefore(response, action);
+
             pWins = 0, cWins = 0;
+            
+            computerGraphText.textContent = "";
+            computerGraph.style.cssText = `flex: ${cWins} 1 auto;`;
         } 
         else if (cWins == 5) {
             response.textContent = "The computer has won this match!";
+            container.insertBefore(response, action);
+
             pWins = 0, cWins = 0;
-        }
-        else {
-            container.removeChild(playerScore);
-            container.removeChild(computerScore);
-            playerScore.textContent = "Player Score: " + pWins;
-            computerScore.textContent = "Computer Score: " + cWins;
-            container.appendChild(playerScore);
-            container.appendChild(computerScore);
+
+            playerGraphText.textContent = "";
+            playerGraph.style.cssText = `flex: ${pWins} 1 auto;`;
         }
     });
 });
